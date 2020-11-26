811dd7a0bad0f8f6f37d7627b2e02ed90d2ca00d
yangfeng <yangfengzzz@hotmail.com>
2020-11-26 12:42:45 +0800

    add model files

---
+++ apps/hello_model/fs_body.sc
@@ -0,0 +1,17 @@
+$input v_texcoord0
+
+/*
+ * Copyright 2011-2020 Branimir Karadzic. All rights reserved.
+ * License: https://github.com/bkaradzic/bgfx#license-bsd-2-clause
+ */
+
+#include "../common/common.sh"
+
+uniform vec4 u_time;
+SAMPLER2D(s_body_diff, 0);
+SAMPLER2D(s_body_spec, 1);
+
+void main()
+{
+	gl_FragColor = texture2D(s_body_diff, v_texcoord0);
+}
---
+++ apps/hello_model/shaderCompile.sh
@@ -0,0 +1,17 @@
+../../../bgfx/.build/osx64_clang/bin/shadercRelease -f fs_arm.sc -o fs_arm.bin -i ../../src.bgfx/bgfx --type f --platform osx -p metal
+../../../bgfx/.build/osx64_clang/bin/shadercRelease -f vs_arm.sc -o vs_arm.bin -i ../../src.bgfx/bgfx --type v --platform osx -p metal
+
+../../../bgfx/.build/osx64_clang/bin/shadercRelease -f fs_body.sc -o fs_body.bin -i ../../src.bgfx/bgfx --type f --platform osx -p metal
+../../../bgfx/.build/osx64_clang/bin/shadercRelease -f vs_body.sc -o vs_body.bin -i ../../src.bgfx/bgfx --type v --platform osx -p metal
+
+../../../bgfx/.build/osx64_clang/bin/shadercRelease -f fs_glass.sc -o fs_glass.bin -i ../../src.bgfx/bgfx --type f --platform osx -p metal
+../../../bgfx/.build/osx64_clang/bin/shadercRelease -f vs_glass.sc -o vs_glass.bin -i ../../src.bgfx/bgfx --type v --platform osx -p metal
+
+../../../bgfx/.build/osx64_clang/bin/shadercRelease -f fs_hand.sc -o fs_hand.bin -i ../../src.bgfx/bgfx --type f --platform osx -p metal
+../../../bgfx/.build/osx64_clang/bin/shadercRelease -f vs_hand.sc -o vs_hand.bin -i ../../src.bgfx/bgfx --type v --platform osx -p metal
+
+../../../bgfx/.build/osx64_clang/bin/shadercRelease -f fs_helmet.sc -o fs_helmet.bin -i ../../src.bgfx/bgfx --type f --platform osx -p metal
+../../../bgfx/.build/osx64_clang/bin/shadercRelease -f vs_helmet.sc -o vs_helmet.bin -i ../../src.bgfx/bgfx --type v --platform osx -p metal
+
+../../../bgfx/.build/osx64_clang/bin/shadercRelease -f fs_leg.sc -o fs_leg.bin -i ../../src.bgfx/bgfx --type f --platform osx -p metal
+../../../bgfx/.build/osx64_clang/bin/shadercRelease -f vs_leg.sc -o vs_leg.bin -i ../../src.bgfx/bgfx --type v --platform osx -p metal
